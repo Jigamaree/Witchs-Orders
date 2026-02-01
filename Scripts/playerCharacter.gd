c50c8d0eb40701052b35a9a809ef8308e2cd3006
Jigamaree <tiger200188@gmail.com>
2026-02-01 23:30:56 +1300

    Basic background, basic grid movement - changing back to all over though, i like it better

--- Scripts/playerCharacter.gd
+++ Scripts/playerCharacter.gd
@@ -1,8 +1,44 @@
 extends CharacterBody2D
 
+var input_dir
+var moving = false
+var tile_size = 50
+@onready var characterSprite = $characterSprite2D
+
 func _physics_process(delta: float) -> void:
-	var input_vector : Vector2 = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"))
-	velocity = input_vector*50000*delta
+	input_dir = Vector2.ZERO
+	var notMoving = false
+	
+	if Input.is_action_pressed("ui_up"):
+		input_dir = Vector2.UP
+		move()
+	if Input.is_action_pressed("ui_down"):
+		input_dir = Vector2.DOWN
+		move()
+	if Input.is_action_pressed("ui_left"):
+		input_dir = Vector2.LEFT
+		move()
+	if Input.is_action_pressed("ui_right"):
+		input_dir = Vector2.RIGHT
+		move()
+	else: notMoving = true
+	
+	# animation control
+	if notMoving == true:
+		characterSprite.play("default")
+	else:
+		characterSprite.play("walk")
 	move_and_slide()
+	
+func move():
+	if input_dir:
+		if moving == false:
+			moving = true
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + input_dir*tile_size, 1)
+			tween.set_speed_scale(25)
+			tween.tween_callback(move_false)
+			
+func move_false():
+	moving = false
+	
