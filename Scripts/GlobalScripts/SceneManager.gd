71370b0250b91de4b1918a05e5ec22354bc3d9e5
Jigamaree <tiger200188@gmail.com>
2026-02-03 03:44:43 +1300

    Technically moves scene? But weird collision stuff going on.

--- Scripts/GlobalScripts/SceneManager.gd
+++ Scripts/GlobalScripts/SceneManager.gd
@@ -4,11 +4,15 @@ class_name Scene_Manager
 
 var previousRoom: GlobalVariables.roomsInHouse
 
+var currentScene: Node = null
+
 @export var bathroomScene: PackedScene 	= preload("res://Scenes/Room_Bathroom.tscn")
 @export var bedroomScene: PackedScene	= preload("res://Scenes/Room_Bedroom.tscn")
 
+func _ready() -> void:
+	switch_scenes(GlobalVariables.roomsInHouse.SLEEPYROOM, -1)
+
 func switch_scenes(sceneFrom: GlobalVariables.roomsInHouse, sceneTo: GlobalVariables.roomsInHouse) -> void:
-	
 	previousRoom = sceneFrom	
 	var nextSceneUp = null
 	
@@ -19,6 +23,25 @@ func switch_scenes(sceneFrom: GlobalVariables.roomsInHouse, sceneTo: GlobalVaria
 		GlobalVariables.roomsInHouse.SLEEPYROOM:	
 			nextSceneUp = bedroomScene
 			
+	if nextSceneUp == null: nextSceneUp = bedroomScene
+			
 	GlobalVariables.emit_signal("changingScene", sceneFrom, sceneTo)
-	get_tree().change_scene_to_packed(nextSceneUp)
+
+	if currentScene:
+		currentScene.queue_free()
 	
+	# instantiate and add the new scene
+	currentScene = nextSceneUp.instantiate()
+	get_tree().root.add_child(currentScene)
+	
+	# move the player to the correct position
+	var player: CharacterBody2D = currentScene.get_node("PlayerCharacter")
+
+	if currentScene.has_node("Markers/MarkerFromBathroom") and previousRoom == GlobalVariables.roomsInHouse.BATHROOM:
+		print("you came from the bathroom!")
+		var marker: Marker2D = currentScene.get_node("Markers/MarkerFromBathroom")
+		player.global_position = marker.global_position
+		
+		
+		#GlobalVariables.emit_signal("moveThePlayer", marker_pos)
+		
--- Scripts/roomScript_default.gd
+++ Scripts/roomScript_default.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 	GlobalVariables.changingScene.connect(_on_changing_scenes)
 	
 func _on_changing_scenes(sceneFrom: GlobalVariables.roomsInHouse, sceneTo: GlobalVariables.roomsInHouse) -> void:
-	print("Going from: " + GlobalVariables.roomToString(sceneFrom) + " to " + GlobalVariables.roomToString(sceneTo))
+	#print("Going from: " + GlobalVariables.roomToString(sceneFrom) + " to " + GlobalVariables.roomToString(sceneTo))
+	var x = 1
