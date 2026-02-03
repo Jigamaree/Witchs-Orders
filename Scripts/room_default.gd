363a7b46b24e9fc6c3c5686c0e6b92241599fe18
Jigamaree <tiger200188@gmail.com>
2026-02-04 00:02:56 +1300

    DOORS WORK.

--- Scripts/room_default.gd
+++ Scripts/room_default.gd
@@ -7,12 +7,15 @@ func _ready():
 	# Spawn player
 	player = player_scene.instantiate()
 	add_child(player)
-	if NavMan.changed_scene_before == true:
-		find_player_location()
+	find_player_location()
 	#else call player spawn on fallback place
 	
 func find_player_location():
-	for door: interactableDoor in get_tree().get_nodes_in_group("door"):
-		if door.currentRoom == NavMan.scene_were_currently_instancing:
-			player.global_position = door.global_position
-			return
+	if NavMan.changed_scene_before == true:
+		for door: interactableDoor in get_tree().get_nodes_in_group("door"):
+			if door.currentRoom == NavMan.scene_were_currently_instancing:
+				player.global_position = door.markerPos
+				return
+	else:
+		for marker: Marker2D in get_tree().get_nodes_in_group("fallback"):
+			player.global_position = marker.global_position
