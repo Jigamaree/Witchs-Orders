d76dd346a7ec2bea82fa9e6b263664994cf0bebd
Jigamaree <tiger200188@gmail.com>
2026-03-01 18:30:00 +1300

    Taking shirt removes shirt.

--- Scripts/RoomScripts/room_bathroom.gd
+++ Scripts/RoomScripts/room_bathroom.gd
@@ -5,3 +5,8 @@ class_name bathroomScript
 func _ready():
 	super._ready()
 	dialogueDictionary = bathroomConvos.convos_Dict
+
+func _re_enable_pausing(): 	
+	super._re_enable_pausing()
+	if SaveManager.getSaveVariable("knight_clothing") == SaveData_CurrentGame.Clothing.POET_SHIRT:
+		$RoomItems/Shirt.visible = false
